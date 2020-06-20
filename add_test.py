5da1906b30538c66f24c7fa74d462ecba2a04354
Nishad Gupta <nishadgupta161@gmail.com>
2020-06-20 18:31:49 +0530

    Mail commit 1

--- add_test.py
+++ add_test.py
@@ -7,4 +7,9 @@ class TestStringMethods(unittest.TestCase):
     def test_add(self):
         d = addition(3, 8)
         d = str(d)
+        print("Hello")
         self.assertEqual(d, '11')
+
+
+if __name__ == '__main__':
+    unittest.main()
